970aef4ec4ac40115251bece28ee612722890de2
Syksy <teemu.daniel.laajala@gmail.com>
2026-01-12 11:53:14 +0200

    Adding support for coxph-objects and classs specifity for coxambi

--- R/coxmodels.R
+++ R/coxmodels.R
@@ -1,18 +1,32 @@
 # Functions related to Cox proportional hazards models
 
 #' Fit multivariable Cox ph model for all covariates together with univariable model once per each covariate
-#' @export coxambi
-coxambi <- function(
+#' @export
+coxambi <- function(obj, ...) {
+	UseMethod("coxambi")
+}
+
+#' Not a suitable class provided as first parameter for coxambi
+#' @export
+coxambi.default <- function(obj, ...) {
+	stop("Unsupported input type for 'coxambi()'. Provide a formula or a survival::coxph object.")
+}
+
+#' Wrapper for a formula + data formulation of Cox model
+#' @export
+coxambi.formula <- function(
 	# Left hands side (lhs) is the survival response, right hand side (rhs) are the covariates
-	formula, 
+	obj, 
 	# Data used for fitting
 	data, 
 	# Parameters passed on to both the coxph call in multivariable and univariable models
 	... 
 ){
+	formula <- obj
 	if(!inherits(formula, "formula")){
 		stop("First parameter should be of 'formula' class")
 	}
+	if (missing(data)) stop("'data' must be provided.")
 	
 	# Fit the multivariable model
 	coxmulti <- survival::coxph(formula, data = data, ...)
@@ -38,4 +52,40 @@ coxambi <- function(
 		coxmulti,
 		coxunis
 	)
+}
+
+#' Wrapper for a readily fitted Cox model and perform single-variable fits per each fitted covariate
+#' @export
+coxambi.coxph <- function(
+	# Fitted coxph-object
+	obj,
+	...
+){
+	if (is.null(obj$model)) {
+		stop("Need 'obj$model' for re-fitting. Refit with survival::coxph(..., model = TRUE).")
+	}
+	combs <- coxambi(obj$formula, data = obj$model)
+	combs
+}
+
+#' Fetch summary of Cox model coefficients in data.frame class format
+#' @export
+coxdf <- function(
+	# A Cox PH model object
+	model
+){
+	# Extract summary
+	summ <- summary(model)
+	df <- data.frame(
+		term  = rownames(summ$coef),
+		coef  = unname(s$coef[,"coef"]),
+		hr    = unname(s$coef[,"exp(coef)"]),
+		se    = unname(s$coef[,"se(coef)"]),
+		z     = unname(s$coef[,"z"]),
+		p     = unname(s$coef[,"Pr(>|z|)"]),
+		dw95  = unname(s$coef[,"lower .95"]),
+		up95  = unname(s$coef[,"upper .95"]),
+		stringsAsFactors = FALSE
+	)
+	df
 }
